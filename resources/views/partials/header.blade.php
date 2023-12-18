7bf8cda6874f8b7bcc1b8e9274af6ddeb10e4d55
Eecodev <mnasser.bbk@gmail.com>
2023-12-18 17:47:55 +0100

    git update

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,21 @@
-<header>Header</header>
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container justify-content-between">
+            <img src="{{asset('')}}" alt="">
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">CHARACTERS</a>
+                    <a class="nav-link" href="#">COMICS</a>
+                    <a class="nav-link" href="#">MOVIES</a>
+                    <a class="nav-link" href="#">TV</a>
+                    <a class="nav-link" href="#">GAMES</a>
+                    <a class="nav-link" href="#">COLLECTIBLES</a>
+                    <a class="nav-link" href="#">VIDEOS</a>
+                    <a class="nav-link" href="#">FANS</a>
+                    <a class="nav-link" href="#">NEWS</a>
+                    <a class="nav-link" href="#">SHOP</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
