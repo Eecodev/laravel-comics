0f2278deafd579cd8d821f070bc06de9370c218c
Eecodev <mnasser.bbk@gmail.com>
2023-12-19 15:19:03 +0100

    header updated

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,21 +1,37 @@
-<header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container">
-            <img class="logo-head" src="{{Vite::asset('/resources/img/dc-logo.png')}}" alt="">
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="#">CHARACTERS</a>
-                    <a class="nav-link" href="#">COMICS</a>
-                    <a class="nav-link" href="#">MOVIES</a>
-                    <a class="nav-link" href="#">TV</a>
-                    <a class="nav-link" href="#">GAMES</a>
-                    <a class="nav-link" href="#">COLLECTIBLES</a>
-                    <a class="nav-link" href="#">VIDEOS</a>
-                    <a class="nav-link" href="#">FANS</a>
-                    <a class="nav-link" href="#">NEWS</a>
-                    <a class="nav-link" href="#">SHOP</a>
-                </div>
-            </div>
-        </div>
-    </nav>
+<header class="container my-4">
+    <div class="d-flex justify-content-between align-items-center">
+        <img class="logo-head" src="{{Vite::asset('/resources/img/dc-logo.png')}}" alt="">
+        <ul class="navbar nav">
+            <li class="nav-item">
+                 <a class="nav-link active" aria-current="page" href="#">CHARACTERS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">COMICS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">MOVIES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">TV</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">GAMES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">COLLECTIBLES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">VIDEOS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">FANS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">NEWS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">SHOP</a>
+            </li>
+        </ul>
+    </div>
 </header>
